86ef73a3c6e0bd67bd200ebc05a27f6848a45130
Xing <Virgo_2333@163.com>
2020-06-05 12:17:28 +0800

    修改签到信息获取方式，提高兼容性

--- cloud189/api/core.py
+++ cloud189/api/core.py
@@ -992,7 +992,8 @@ class Cloud189(object):
         if resp.status_code != requests.codes.ok:
             print(f"签到失败 {resp=}, {headers=}")
         else:
-            msg = re.findall(r'获得.+空间', resp.text)[0]
+            msg = re.search(r'获得.+?空间', resp.text)
+            msg = msg.group() if msg else "" 
             print(f"签到成功！{msg}。每天签到可领取更多福利哟，记得常来！)
 
         url = 'https://m.cloud.189.cn/v2/drawPrizeMarketDetails.action'
